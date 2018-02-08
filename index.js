c2775bc2c4d50d42ab16a2538aa21bb230567369
Mary Prier <priertroyandmary@gmail.com>
2018-02-08 15:45:00 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a,b) {
+  return a + b
+}
+function subtract(b,a) {
+  return b - a
+}
+function multiply(a,b) {
+  return a * b
+}
+function divide(a,b) {
+  return a / b
+}
+function inc(a) {
+  return a + 1
+}
+function dec(a) {
+  return a - 1
+}
+function makeInt(n) {
+  return n
+}
+ var makeInt = function(value) {
+   if (0 * 2328)$/test(value)
+   return Number(value);
+   return NaN;
+ }
